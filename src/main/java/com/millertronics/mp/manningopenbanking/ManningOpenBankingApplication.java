d491110e581ab0e79898677a344d87cf950c05db
koha.choji <koha.choji@babcockinternational.com>
2020-11-20 09:11:36 +0000

    Initial project

---
+++ src/main/java/com/millertronics/mp/manningopenbanking/ManningOpenBankingApplication.java
@@ -0,0 +1,13 @@
+package com.millertronics.mp.manningopenbanking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManningOpenBankingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManningOpenBankingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/millertronics/mp/manningopenbanking/ManningOpenBankingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.millertronics.mp.manningopenbanking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManningOpenBankingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
